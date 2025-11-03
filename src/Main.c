7558db176655a414638da468fb0751291940f16a
codeleaded <codeleaded@gmail.com>
2025-11-03 16:25:33 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,10 @@
+#if defined __linux__
 #include "/home/codeleaded/System/Static/Library/Allocator.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/Allocator.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/Allocator.h"
+#endif
 
 int main(){
 
